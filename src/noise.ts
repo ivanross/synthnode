d2ccb59d82ae416bdbbc87c0e544d616a7d105d4
Ivan <ivanross.me@gmail.com>
2019-02-24 19:19:54 +0100

    noise module added

---
+++ src/noise.ts
@@ -0,0 +1,14 @@
+import { AudioObject, toAudioObject } from './audio-object';
+
+export class Noise implements AudioObject {
+  private amplitude: AudioObject;
+  constructor(amplitude: number | AudioObject = 1) {
+    this.amplitude = toAudioObject(amplitude);
+  }
+  setAmplitude(value: number) {
+    this.amplitude = toAudioObject(value);
+  }
+  tf(t: number): number {
+    return this.amplitude.tf(t) * Math.random() * 2 - 1;
+  }
+}
