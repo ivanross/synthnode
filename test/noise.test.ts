41faa60dcfff67a3965f45cefd4c843479039f7a
Ivan Rossi <ivan.rossi95@gmail.com>
2019-04-23 23:49:12 +0200

    ⬆️ 👷 update dependencies + noise tests

--- test/noise.test.ts
+++ test/noise.test.ts
@@ -1,19 +1,31 @@
 import 'mocha';
 import { expect } from 'chai';
-import { Noise } from '../dist';
+import { Noise, noise, isNoise, isAudioObject } from '../dist';
 
-describe('noise', () => {
+describe('Noise', () => {
+  it('noise constructor', () => {
+    expect(() => new Noise()).to.not.throw();
+    expect(() => noise()).to.not.throw();
+  });
+
+  it('noise type guard', () => {
+    expect(isNoise(new Noise())).to.be.true;
+    expect(isNoise(noise())).to.be.true;
+    expect(isAudioObject(noise())).to.be.true;
+  });
+
   it('noise properties', () => {
-    const n = new Noise();
+    const n = noise();
     expect(n).to.have.property('amplitude');
     expect(n).to.have.property('tf');
   });
 
-  it('amplitude changes', () => {
-    const n = new Noise();
+  it('amplitude', () => {
+    const n = noise();
     expect(() => n.amplitude(0.5)).to.not.throw();
+    expect(isAudioObject(n.amplitude())).to.be.true;
     expect(n.tf(0)).to.not.be.NaN;
-    n.amplitude('ciao' as any);
+    n.amplitude('hello world' as any);
     expect(n.tf(0)).to.be.NaN;
   });
 });
